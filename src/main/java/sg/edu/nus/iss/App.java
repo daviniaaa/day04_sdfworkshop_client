0b34de6df26a2645eceed865813abc11367e30ca
daviniaaa <daviniaho99@gmail.com>
2023-04-21 08:44:10 +0800

    second commit

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,9 +1,14 @@
 package sg.edu.nus.iss;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 /**
@@ -14,20 +19,66 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
+
+        // console input from keyboard
+        // variable to save keyboard inputs
+        // variable to save msgReceived
+        Console cons = System.console();
+        String input = "";
+        String msgReceived = "";
+        
         // args[0] for port number
-        String port = args[0];
+        String serverHost = args[0];
+        String serverPort = args[1];
 
         // slide 8 - establish server connection
-        Integer portNum = Integer.parseInt(port);
-        Socket socket = new Socket("localhost", portNum);
+        // ** server side must be started first
+        Integer portNum = Integer.parseInt(serverPort);
+        Socket socket = new Socket(serverHost, portNum);
 
         try (InputStream is = socket.getInputStream()) {
             BufferedInputStream bis = new BufferedInputStream(is);
             DataInputStream dis = new DataInputStream(bis);
-            String line = dis.readUTF();
 
-            System.out.println(line);
+            try (OutputStream os = socket.getOutputStream()) {
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(os);
+
+                // while loop to keep going
+                while(!input.equals("quit")) {
+                    input = cons.readLine(">> ");
+                    if (!input.equals("get-cookie")) {
+                        continue;
+                    }
+
+                    // send message out across communication tunnel
+                    dos.writeUTF(input);
+                    dos.flush();
+
+                    // receive message from server (the response to client input) and process it
+                    String line = dis.readUTF();
+                    System.out.println(line);
+                    
+
+                }
+                
+
+
+                dos.close();
+                bos.close();
+                os.close();
+
+            } catch (EOFException e) {
+                
+            }
+
+            dis.close();
+            bis.close();
+            is.close();
         }
+
+
+        socket.close();
 
     }
 }
